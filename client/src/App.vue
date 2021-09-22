b69a2ef165261dceceee0f8a18570b127246bc09
Filip <wojtasik.f03@htlwienwest.at>
2021-09-22 13:40:43 +0200

    Coca Cola Logo dings

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <v-app>
-    <v-navigation-drawer class="green" app>
-      <span class="white--text title">v-navigation-drawer</span>
-    </v-navigation-drawer>
-    <v-system-bar class="pink" app>
-      <span class="white--text title">v-system-bar</span>
-    </v-system-bar>
-    <v-app-bar class="yellow" app>
-      <span class="title">v-app-bar</span>
+    <v-app-bar class="blue lighten-5" app>
+      <span class="text-h4">Drink</span>
+      <v-img class="mx-2" src="@/images/logo.png" max-height="140" max-width="140" contain></v-img>
     </v-app-bar>
-    <v-main class="blue">
-      <span class="white--text title">v-main</span>
-    </v-main>
-    <v-footer class="purple" app>
-      <span class="white--text title mx-auto">v-footer</span>
-    </v-footer>
-    <v-bottom-navigation class="cyan" app>
-      <span class="white--text title">v-bottom-navigation</span>
-    </v-bottom-navigation>
+    <v-main class="blue"> </v-main>
   </v-app>
 </template>
 
