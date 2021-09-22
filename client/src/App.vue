4d2e247b588d130081cb83840692cdba7e16fd97
Filip <wojtasik.f03@htlwienwest.at>
2021-09-22 23:03:43 +0200

    Aufgabe 4

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,36 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar class="blue" app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="white--text"></v-app-bar-nav-icon>
-      <span class="white--text title">Layout Demo</span>
+    <v-app-bar class="grey lighten-5" height="74" app>
+      <img class="mx-2" src="@/images/BMW.svg.png" style="height:150px; margin-top:100px" />
+      <span class="text-truncate text-h4 blue-grey--text darken-1" style="font-family: Poppins"
+        >A Car With a Mission</span
+      >
+      <v-spacer></v-spacer>
+      <div class="hidden-md-and-down">
+        <v-btn active-class="white--text red" class="white--text black" to="/" exact>Home</v-btn>
+        <v-btn active-class="white--text red" class="white--text black ml-5" to="/products" exact
+          >Products</v-btn
+        >
+        <v-btn active-class="white--text red" class="white--text black ml-5" to="/about" exact>About</v-btn>
+      </div>
+
+      <v-app-bar-nav-icon
+        v-if="!drawer"
+        @click.stop="drawer = !drawer"
+        class="black--text hidden-lg-and-up"
+      ></v-app-bar-nav-icon>
     </v-app-bar>
-    <v-navigation-drawer app hide-overlay disable-resize-watcher v-model="drawer">
+
+    <v-navigation-drawer app hide-overlay disable-resize-watcher v-model="drawer" right>
       <v-row>
         <v-col cols="6">
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title class="text-h6"> Menu </v-list-item-title>
-              <v-list-item-subtitle style="grey"> Please select</v-list-item-subtitle>
+              <v-list-item-title class="text-h6">
+                Menu
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Please select
+              </v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-col>
-
         <v-col cols="6">
           <v-btn text @click="drawer = false" class="mt-4" float="right">Close</v-btn>
         </v-col>
       </v-row>
-      <v-divider> </v-divider>
 
-      <v-list-item-group active-class="blue white--text">
+      <v-divider></v-divider>
+      <v-list-item-group active-class="red white--text">
         <v-list-item link v-for="(route, i) in routes" :key="i" :to="route.link">
           {{ route.name }}
         </v-list-item>
       </v-list-item-group>
     </v-navigation-drawer>
-    <v-main class="yellow"> </v-main>
+    <v-main>
+      <div class="d-flex">
+        <div style="width:200px"></div>
+        <div class="mt-10" style="flex:1"><router-view></router-view></div>
+      </div>
+    </v-main>
   </v-app>
 </template>
-<!-- @Professor alles ist verbunden, aber der Text aus den Views wird aus irgendeinem Grund nicht angezeigt-->
 <script>
 export default {
   name: 'App',
--- client/src/views/Team.vue
+++ client/src/views/Team.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Team">
-        <h1 class="blue">Team dings</h1>
+        <h1>Team dings</h1>
     </div>
 </template>
 
